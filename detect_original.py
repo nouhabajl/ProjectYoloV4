680c18b2bfc32ba2dd949c6dc4d11513543804a5
Nouha Ben Abdeljelil <43712658+nouhabajl@users.noreply.github.com>
2021-06-01 09:51:12 +0100

    Update detect_original.py
    Forth commit

--- detect_original.py
+++ detect_original.py
@@ -30,7 +30,7 @@ flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
 flags.DEFINE_string('image', './data/kite.jpg', 'path to input image')
 flags.DEFINE_string('output', 'result.png', 'path to output image')
 flags.DEFINE_float('iou', 0.45, 'iou threshold')
-flags.DEFINE_float('score', 0.25, 'score threshold')
+flags.DEFINE_float('score', 0.45, 'score threshold')
 
 
 def main(_argv):
