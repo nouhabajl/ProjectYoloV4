dbad537a73e6bd453acb05e390f78b6710ee9187
Nouha Ben Abdeljelil <43712658+nouhabajl@users.noreply.github.com>
2021-06-01 09:35:18 +0100

    Update detect_original.py
    Third commit

--- detect_original.py
+++ detect_original.py
@@ -4,7 +4,7 @@ if len(physical_devices) > 0:
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 from absl import app, flags, logging
 from absl.flags import FLAGS
-import util as utils
+import utils as utils
 from core.yolov4 import filter_boxes
 from tensorflow.python.saved_model import tag_constants
 from PIL import Image
